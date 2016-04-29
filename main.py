37a011ce43016dbabfdc4de1c8f5f10bce667abe
Veltin Dupont <vdupont@octo.com>
2016-04-29 15:24:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import time
+import zmq
+
+
+def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
+
+    while True:
+        #  Wait for next request from client
+        print("Waiting for message...")
+        message = socket.recv()
+        print("Received request: %s" % message)
+
+        #  Do some 'work'
+        time.sleep(1)
+
+        #  Send reply back to client
+        socket.send(b"World")
+
+if __name__ == '__main__':
+    main()
